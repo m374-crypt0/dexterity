ed2d3ce5520f296b311035ce46d3097a84657db8
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:56 +0000

    feat: WalletConnect

--- frontend/src/app/header/wallet_connect/index.tsx
+++ frontend/src/app/header/wallet_connect/index.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import useWalletConnect from "./use_wallet_connect"
+import { BrowserProvider, JsonRpcSigner } from "ethers";
+import { useEffect, useState } from "react";
+import useWalletConnect, { NetworkInfo } from "./use_wallet_connect";
 
 export default () => {
-  const use = useWalletConnect();
+  const [provider, setProvider] = useState<BrowserProvider>();
+  const [connectedNetwork, setConnectedNetwork] = useState<NetworkInfo>({ name: "Disconnected", chainId: 0n });
+  const [connectedAccount, setConnectedAccount] = useState("Connect");
+  const [accountBalance, setAccountBalance] = useState("0 ETH");
+  const [signer, setSigner] = useState<JsonRpcSigner>();
+
+  const use = useWalletConnect({
+    provider, setProvider,
+    setConnectedAccount,
+    connectedNetwork, setConnectedNetwork,
+    setAccountBalance,
+    setSigner
+  });
+
+  useEffect(use.plugWallet, []);
+  useEffect(use.updateWalletStates, [provider]);
 
   return (
     <div className="flex justify-self-end
       bg-[var(--wallet-connect-disconnected-background)] mr-1 mt-1 rounded-md
       p-1 items-center justify-center cursor-pointer"
-      onClick={use.handleClick}>
-      Connect
+      style={{ backgroundColor: `var(--wallet-connect-${connectedNetwork.chainId ? 'connected' : 'disconnected'}-background)` }}
+      onClick={use.connectAndPlugWallet}>
+      {connectedAccount}
     </div>
   );
 }
--- frontend/src/app/header/wallet_connect/use_wallet_connect.ts
+++ frontend/src/app/header/wallet_connect/use_wallet_connect.ts
@@ -1,7 +1,96 @@
-const useWalletConnect = () => {
+import { BrowserProvider, JsonRpcSigner } from "ethers";
+import { ethers } from "ethers";
+import { Dispatch, SetStateAction } from "react";
+
+export type NetworkInfo = {
+  name: string,
+  chainId: bigint
+};
+
+export default function (states: States) {
   return {
-    handleClick: () => { }
+    connectAndPlugWallet: async () => {
+      await switchWalletNetworkToSepolia();
+      plugBrowserWallet();
+    },
+    updateWalletStates: () => {
+      (async () => {
+        if (!states.provider)
+          return;
+
+        const network = await states.provider.getNetwork();
+        const signers = await states.provider.listAccounts();
+
+        // It may fail because of disconnected wallet
+        if (signers.length === 0)
+          return;
+
+        const signer = signers[0];
+        const address = await signer.getAddress();
+        const balance = await states.provider.getBalance(signer);
+
+        states.setConnectedAccount(formatAddress(address));
+        states.setConnectedNetwork({ name: network.name, chainId: network.chainId });
+        states.setAccountBalance(formatBalance(balance));
+        states.setSigner(signer);
+      })();
+    },
+    plugWallet: () => { plugBrowserWallet(); }
   };
+
+  function plugBrowserWallet() {
+    if (!window.ethereum) {
+      // TODO: error handling
+      // props.setErrorMessage("You need to install a MetaMask compatible wallet");
+
+      return;
+    }
+
+    // Plug the wallet as it is, disconnected or connected
+    states.setProvider(new ethers.BrowserProvider(window.ethereum));
+  }
+
+  async function switchWalletNetworkToSepolia() {
+    if (!window.ethereum) {
+      // TODO: error handling
+      // props.setErrorMessage("You need to install a MetaMask compatible wallet");
+
+      return;
+    }
+
+    if (states.connectedNetwork?.chainId === 11155111n)
+      return;
+
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{
+          chainId: `0x${11155111n.toString(16)}`
+        }]
+      });
+    }
+    catch (error) {
+      if (error instanceof Object && "message" in error && typeof error.message === "string") {
+        // props.setErrorMessage(error.message);
+      }
+    }
+  }
+};
+
+function formatAddress(address: string): string {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`
 }
 
-export default useWalletConnect;
+function formatBalance(amount: bigint): string {
+  return `${ethers.formatEther(amount)} ETH`;
+}
+
+type States = {
+  provider: BrowserProvider | undefined,
+  setProvider: Dispatch<SetStateAction<BrowserProvider | undefined>>,
+  setConnectedAccount: Dispatch<SetStateAction<string>>,
+  connectedNetwork: NetworkInfo | undefined,
+  setConnectedNetwork: Dispatch<SetStateAction<NetworkInfo>>,
+  setAccountBalance: Dispatch<SetStateAction<string>>,
+  setSigner: Dispatch<SetStateAction<JsonRpcSigner | undefined>>
+};
