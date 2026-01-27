361ab4e4a6b80f41f75b4475ddda45302821b1b1
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:56 +0000

    refactor: update dropdown position logic

--- frontend/src/app/header/menus/feature_menus/index.tsx
+++ frontend/src/app/header/menus/feature_menus/index.tsx
@@ -7,40 +7,21 @@ import Pool from "./pool";
 import PoolDropdown from "./pool_dropdown";
 import Trade from "./trade";
 import TradeDropdown from "./trade_dropdown";
-import { DisplayedDropdown } from "./use_feature_menus"
 import { DropdownPosition } from "./trade_dropdown/use_trade_dropdown";
+import { DisplayedDropdown, setupDropdownPositionUpdates } from "./use_feature_menus";
 
 export default () => {
   const [displayedDropdown, setDisplayedDropdown] = useState<DisplayedDropdown>();
   const [dropdownPosition, setDropdownPosition] = useState<DropdownPosition>();
 
-  const menuRef = useRef<HTMLDivElement>(undefined!);
+  const parentMenuRef = useRef<HTMLDivElement>(undefined!);
 
-  useEffect(() => {
-    const updateDropdownPosition = () => {
-      if (!menuRef.current)
-        return;
-
-      const rect = menuRef.current.getBoundingClientRect();
-
-      setDropdownPosition({ top: rect.bottom + window.scrollY, left: rect.left });
-    }
-
-    updateDropdownPosition();
-
-    window.addEventListener("resize", updateDropdownPosition);
-    window.addEventListener("scroll", updateDropdownPosition);
-
-    return () => {
-      window.removeEventListener("resize", updateDropdownPosition);
-      window.removeEventListener("scroll", updateDropdownPosition);
-    };
-  }, []);
+  useEffect(setupDropdownPositionUpdates({ parentMenuRef, setDropdownPosition }), []);
 
   return (
     <div className="ml-8 flex">
       <div className="grid grid-cols-3 gap-4 mt-1 font-black">
-        <Trade ref={menuRef} setDisplayedDropdown={setDisplayedDropdown} />
+        <Trade ref={parentMenuRef} setDisplayedDropdown={setDisplayedDropdown} />
         <Explore setDisplayedDropdown={setDisplayedDropdown} />
         <Pool setDisplayedDropdown={setDisplayedDropdown} />
 
--- frontend/src/app/header/menus/feature_menus/use_feature_menus.ts
+++ frontend/src/app/header/menus/feature_menus/use_feature_menus.ts
@@ -1,7 +1,34 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, RefObject, SetStateAction } from "react";
+import { DropdownPosition } from "./trade_dropdown/use_trade_dropdown";
 
 export type DisplayedDropdown = "Explore" | "Pool" | "Trade";
 
 export type Props = {
   setDisplayedDropdown: Dispatch<SetStateAction<DisplayedDropdown | undefined>>
 };
+
+export const setupDropdownPositionUpdates = (args: {
+  parentMenuRef: RefObject<HTMLDivElement>,
+  setDropdownPosition: Dispatch<SetStateAction<DropdownPosition | undefined>>
+}) => {
+  return () => {
+    const updateDropdownPosition = () => {
+      if (!args.parentMenuRef.current)
+        return;
+
+      const rect = args.parentMenuRef.current.getBoundingClientRect();
+
+      args.setDropdownPosition({ top: rect.bottom + window.scrollY, left: rect.left });
+    }
+
+    updateDropdownPosition();
+
+    window.addEventListener("resize", updateDropdownPosition);
+    window.addEventListener("scroll", updateDropdownPosition);
+
+    return () => {
+      window.removeEventListener("resize", updateDropdownPosition);
+      window.removeEventListener("scroll", updateDropdownPosition);
+    };
+  }
+}
