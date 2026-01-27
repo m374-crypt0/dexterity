64995757757d597ac571d857c08458fb5605a742
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:57 +0000

    refactor: clumsy addresses

--- contracts/test/Dexterity.t.sol
+++ contracts/test/Dexterity.t.sol
@@ -9,14 +9,18 @@ import { TokenB } from "./ERC20/TokenB.sol";
 import { Test, console } from "forge-std/Test.sol";
 
 contract DexterityTests is Test {
-  Dexterity public dex;
+  Dexterity dex;
   TokenA tokenA;
   TokenB tokenB;
+  address tokenAAddress;
+  address tokenBAddress;
 
   function setUp() public {
     dex = new Dexterity();
     tokenA = new TokenA();
     tokenB = new TokenB();
+    tokenAAddress = address(tokenA);
+    tokenBAddress = address(tokenB);
   }
 
   function test_deploy_creatorIsSet() public view {
@@ -32,20 +36,20 @@ contract DexterityTests is Test {
   function test_createERC20Pair_fails_WithSameTokenAddress() public {
     vm.expectRevert(IDexterity.CreateERC20OnlyPairSameAddress.selector);
 
-    dex.createERC20OnlyPair(address(tokenA), address(tokenA));
+    dex.createERC20OnlyPair(tokenAAddress, tokenAAddress);
   }
 
   function test_createERC20Pair_returnsPairId_WithValidTokenAddresses() public {
-    uint256 pairId = dex.createERC20OnlyPair(address(tokenA), address(tokenB));
+    uint256 pairId = dex.createERC20OnlyPair(tokenAAddress, tokenBAddress);
 
     assertNotEq(uint256(0), pairId);
   }
 
   function test_createERC20Pair_fails_whenPairAlreadyExists() public {
-    dex.createERC20OnlyPair(address(tokenA), address(tokenB));
+    dex.createERC20OnlyPair(tokenAAddress, tokenBAddress);
 
     vm.expectRevert(IDexterity.CreateERC20OnlyPairAlreadyExists.selector);
-    dex.createERC20OnlyPair(address(tokenB), address(tokenA));
+    dex.createERC20OnlyPair(tokenBAddress, tokenAAddress);
   }
 
   function test_createERC20EtherPair_fails_withZeroTokenAddress() public {
@@ -54,41 +58,40 @@ contract DexterityTests is Test {
   }
 
   function test_createERC20EtherPair_returnsPairId_withValidTokenAddress() public {
-    uint256 pairId = dex.createERC20EtherPair(address(tokenA));
+    uint256 pairId = dex.createERC20EtherPair(tokenAAddress);
 
     assertNotEq(uint256(0), pairId);
   }
 
   function test_createERC20EtherPair_fails_whenPairAlreadyExists() public {
-    dex.createERC20EtherPair(address(tokenA));
+    dex.createERC20EtherPair(tokenAAddress);
 
     vm.expectRevert(IDexterity.CreateERC20EtherPairAlreadyExists.selector);
-    dex.createERC20EtherPair(address(tokenA));
+    dex.createERC20EtherPair(tokenAAddress);
   }
 
   function test_createERC20OnlyPair_emitsERC20OnlyPairCreated_withValidPair() public {
-    uint256 pairId = uint256(keccak256(abi.encodePacked(address(tokenA), address(tokenB))));
+    uint256 pairId = uint256(keccak256(abi.encodePacked(tokenAAddress, tokenBAddress)));
 
     vm.expectEmit();
-    emit IDexterity.ERC20OnlyPairCreated(address(tokenA), address(tokenB), pairId);
+    emit IDexterity.ERC20OnlyPairCreated(tokenAAddress, tokenBAddress, pairId);
 
-    dex.createERC20OnlyPair(address(tokenA), address(tokenB));
+    dex.createERC20OnlyPair(tokenAAddress, tokenBAddress);
   }
 
   function test_createERC20EtherPair_emitsERC20EtherPairCreated_withValidPair() public {
-    uint256 pairId = uint256(keccak256(abi.encodePacked(address(tokenA))));
+    uint256 pairId = uint256(keccak256(abi.encodePacked(tokenAAddress)));
 
     vm.expectEmit();
-    emit IDexterity.ERC20EtherPairCreated(address(tokenA), pairId);
+    emit IDexterity.ERC20EtherPairCreated(tokenAAddress, pairId);
 
-    dex.createERC20EtherPair(address(tokenA));
+    dex.createERC20EtherPair(tokenAAddress);
   }
 
-  // TODO: too much address(token)...
   function test_depositERC20Only_fails_withUnhandledToken() public {
-    dex.createERC20OnlyPair(address(tokenA), address(tokenB));
+    dex.createERC20OnlyPair(tokenAAddress, tokenBAddress);
 
     vm.expectRevert(IDexterity.DepositERC20OnlyUnhandledToken.selector);
-    dex.depositERC20Only(address(tokenA), address(tokenB), uint256(0), uint256(0));
+    dex.depositERC20Only(tokenAAddress, tokenBAddress, uint256(0), uint256(0));
   }
 }
