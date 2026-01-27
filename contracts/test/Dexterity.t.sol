5eb04bb95daf9dd671965371dd05137e459377d9
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:56 +0000

    test: still scaffolding

--- contracts/test/Dexterity.t.sol
+++ contracts/test/Dexterity.t.sol
@@ -6,4 +6,20 @@ import { Test, console } from "forge-std/Test.sol";
 
 contract DexterityTest is Test {
   Dexterity public dexterity;
+
+  function setUp() public {
+    dexterity = new Dexterity();
+  }
+
+  function test_failingTest() public pure {
+    console.log("A failing test...");
+
+    assertEq(uint256(123), uint256(456));
+  }
+
+  /*
+   * function testFuzz_fuzzingExample()public{
+   * ...
+   * }
+  */
 }
