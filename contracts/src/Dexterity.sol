14f683f815ad226e35543731fd31acb2b47bf636
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:57 +0000

    test: creator

--- contracts/src/Dexterity.sol
+++ contracts/src/Dexterity.sol
@@ -3,4 +3,10 @@ pragma solidity 0.8.28;
 
 import { IDexterity } from "./interfaces/IDexterity.sol";
 
-contract Dexterity is IDexterity { }
+contract Dexterity is IDexterity {
+  address public immutable creator;
+
+  constructor() {
+    creator = msg.sender;
+  }
+}
--- contracts/test/Dexterity.t.sol
+++ contracts/test/Dexterity.t.sol
@@ -5,37 +5,15 @@ import { Dexterity } from "../src/Dexterity.sol";
 import { Test, console } from "forge-std/Test.sol";
 
 contract DexterityTest is Test {
-  Dexterity public dexterity;
-  address public constant uniswapV2Factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+  Dexterity public dex;
 
   function setUp() public {
-    string memory mainnetRpcUrl = vm.rpcUrl("mainnet");
-    uint256 mainnetFork = vm.createSelectFork(mainnetRpcUrl, vm.envUint("MAINNET_FORK_BLOCK"));
-
-    dexterity = new Dexterity();
+    dex = new Dexterity();
   }
 
-  function test_read_uniswapv2_succeeds() public {
-    (bool success, bytes memory data) = uniswapV2Factory.call(abi.encodeWithSignature("allPairsLength()"));
-
-    uint256 count;
-    assembly {
-      count := mload(add(data, 0x20))
-    }
-
-    assertTrue(success);
-    console.log(count);
+  function test_dexterityCreatorIsSetAtDeployTime() public view {
+    assertEq(dex.creator(), address(this));
   }
 
-  function test_failingTest() public pure {
-    console.log("A failing test...");
-
-    assertEq(uint256(123), uint256(456));
-  }
-
-  /*
-   * function testFuzz_fuzzingExample()public{
-   * ...
-   * }
-  */
+  function test_createPairFailsWithInvalidTokenAddress() public pure { }
 }
