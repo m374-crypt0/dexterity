94b2fddedff8e742903dda0038cf4d9c36b3b5f4
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:57 +0000

    test: created a pair of ERC20 token

---
+++ contracts/test/ERC20/TokenA.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenA is ERC20 {
+  address private owner_;
+
+  error Unauthorized();
+
+  constructor() ERC20("Token A", "TKA") {
+    owner_ = msg.sender;
+  }
+
+  function owner() public view returns (address) {
+    return owner_;
+  }
+
+  function decimals() public pure override returns (uint8) {
+    return 18;
+  }
+
+  function mintFor(address to, uint256 amount) external {
+    require(msg.sender == owner_, Unauthorized());
+
+    _mint(to, amount);
+  }
+
+  function burn(address from, uint256 amount) external {
+    require(msg.sender == owner_, Unauthorized());
+
+    if (amount > totalSupply()) amount = totalSupply();
+
+    _burn(from, amount);
+  }
+}
