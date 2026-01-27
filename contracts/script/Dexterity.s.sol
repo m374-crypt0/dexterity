4b3df9aa21e09f9b5f26d2aefd538b0c5854b07c
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:57 +0000

    build: deploy actions with wallet

--- contracts/script/Dexterity.s.sol
+++ contracts/script/Dexterity.s.sol
@@ -2,15 +2,18 @@
 pragma solidity 0.8.28;
 
 import { Dexterity } from "../src/Dexterity.sol";
+
 import { Script, console } from "forge-std/Script.sol";
+import { Vm } from "forge-std/Vm.sol";
 
 contract DexterityScript is Script {
   function setUp() public { }
 
   function run() public {
     uint256 pk = vm.envUint("PRIVATE_KEY");
+    Vm.Wallet memory wallet = vm.createWallet(pk);
 
-    vm.startBroadcast(pk);
+    vm.startBroadcast(wallet.privateKey);
 
     Dexterity dexterity = new Dexterity();
 
