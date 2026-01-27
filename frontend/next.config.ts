3258ab9897120f38b498d5efdd876491c8bfa3ce
Sebastien Levy <m374-crypt0@pm.me>
2026-01-27 14:15:56 +0000

    chore: export static

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // To export static content
+  // output: "export"
 };
 
 export default nextConfig;
